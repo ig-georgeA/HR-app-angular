7f343ed27994b808cdf3a916229265f60ddc0605
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2023-10-18 17:02:16 -0400

    HR Portal (7) initial version

---
+++ src/app/my-info/my-info.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-info',
+  templateUrl: './my-info.component.html',
+  styleUrls: ['./my-info.component.scss']
+})
+export class MyInfoComponent {
+  public value = 'Erin';
+  public value1 = 'First name';
+  public value2 = 'Brockovich';
+  public value3 = '2';
+  public value4 = '123 Wallaby Lane';
+  public value5 = 'Sacramento';
+  public value6 = 'CA';
+  public value7 = '94203';
+}
